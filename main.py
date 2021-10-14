0e6218c4553d627f9041d12eb95562fc46e5248f
sonerrms <91956369+sonerrms@users.noreply.github.com>
2021-10-14 12:19:39 +0200

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,68 @@
+namespace SpriteKind {
+    export const GolfBall = SpriteKind.create()
+    export const Hole = SpriteKind.create()
+}
+scene.onHitTile(SpriteKind.GolfBall, 0, function (sprite) {
+    golfball.stopDart()
+})
+let golfball: Dart = null
+tiles.setTilemap(tilemap`level1`)
+scene.setTile(14, img`
+    e e e e e e e e e e e e e e e e 
+    e e e e e e e e e e e e e e e e 
+    e e e e e e e e e e e e e e e e 
+    e e e e e e e e e e e e e e e e 
+    e e e e e e e e e e e e e e e e 
+    e e e e e e e e e e e e e e e e 
+    e e e e e e e e e e e e e e e e 
+    e e e e e e e e e e e e e e e e 
+    e e e e e e e e e e e e e e e e 
+    e e e e e e e e e e e e e e e e 
+    e e e e e e e e e e e e e e e e 
+    e e e e e e e e e e e e e e e e 
+    e e e e e e e e e e e e e e e e 
+    e e e e e e e e e e e e e e e e 
+    e e e e e e e e e e e e e e e e 
+    e e e e e e e e e e e e e e e e 
+    `, true)
+scene.setBackgroundColor(7)
+golfball = darts.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . 1 1 1 1 1 . . . . . . 
+    . . . . 1 1 1 1 1 1 1 . . . . . 
+    . . . 1 1 1 1 1 1 1 1 1 . . . . 
+    . . . 1 1 1 1 1 1 1 1 1 . . . . 
+    . . . 1 1 1 1 1 1 1 1 1 . . . . 
+    . . . 1 1 1 1 1 1 1 1 1 . . . . 
+    . . . . 1 1 1 1 1 1 1 . . . . . 
+    . . . . . 1 1 1 1 1 . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.GolfBall, 25, 110)
+golfball.setTrace()
+golfball.controlWithArrowKeys()
+let hole = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . 2 . . . . . . . 
+    . . . . . . . 2 2 . . . . . . . 
+    . . . . . . 2 2 2 . . . . . . . 
+    . . . . . 2 2 2 2 . . . . . . . 
+    . . . . 2 2 2 2 2 . . . . . . . 
+    . . . 2 2 2 2 2 2 . . . . . . . 
+    . . . . . . . . 1 . . . . . . . 
+    . . . . . . . . 1 . . . . . . . 
+    . . . . . . . . 1 . . . . . . . 
+    . . . . . . . . 1 . . . . . . . 
+    . . . . . . 1 1 1 1 1 . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Hole)
+hole.setPosition(960, 105)
+scene.cameraFollowSprite(golfball.sprite)
---
+++ main.ts
@@ -0,0 +1,80 @@
+namespace SpriteKind {
+    export const GolfBall = SpriteKind.create()
+    export const Hole = SpriteKind.create()
+}
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    golfball.throwDart()
+})
+scene.onHitTile(SpriteKind.GolfBall, 14, function (sprite) {
+    golfball.stopDart()
+})
+let golfball: Dart = null
+scene.setTileMap(img`
+    eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+    e.................e......................e.....................e
+    e.................e......................e.....................e
+    e.................e......................e.....................e
+    e........e...................e...................e.............e
+    e........e...................e...................e.............e
+    e........e...................e...................e.............e
+    eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+    `)
+scene.setTile(14, img`
+    e e e e e e e e e e e e e e e e 
+    e e e e e e e e e e e e e e e e 
+    e e e e e e e e e e e e e e e e 
+    e e e e e e e e e e e e e e e e 
+    e e e e e e e e e e e e e e e e 
+    e e e e e e e e e e e e e e e e 
+    e e e e e e e e e e e e e e e e 
+    e e e e e e e e e e e e e e e e 
+    e e e e e e e e e e e e e e e e 
+    e e e e e e e e e e e e e e e e 
+    e e e e e e e e e e e e e e e e 
+    e e e e e e e e e e e e e e e e 
+    e e e e e e e e e e e e e e e e 
+    e e e e e e e e e e e e e e e e 
+    e e e e e e e e e e e e e e e e 
+    e e e e e e e e e e e e e e e e 
+    `, true)
+scene.setBackgroundColor(7)
+golfball = darts.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . 1 1 1 1 1 . . . . . . 
+    . . . . 1 1 1 1 1 1 1 . . . . . 
+    . . . 1 1 1 1 1 1 1 1 1 . . . . 
+    . . . 1 1 1 1 1 1 1 1 1 . . . . 
+    . . . 1 1 1 1 1 1 1 1 1 . . . . 
+    . . . 1 1 1 1 1 1 1 1 1 . . . . 
+    . . . . 1 1 1 1 1 1 1 . . . . . 
+    . . . . . 1 1 1 1 1 . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.GolfBall, 25, 105)
+golfball.setTrace()
+golfball.controlWithArrowKeys()
+let hole = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . 2 . . . . . . . 
+    . . . . . . . 2 2 . . . . . . . 
+    . . . . . . 2 2 2 . . . . . . . 
+    . . . . . 2 2 2 2 . . . . . . . 
+    . . . . 2 2 2 2 2 . . . . . . . 
+    . . . 2 2 2 2 2 2 . . . . . . . 
+    . . . . . . . . 1 . . . . . . . 
+    . . . . . . . . 1 . . . . . . . 
+    . . . . . . . . 1 . . . . . . . 
+    . . . . . . . . 1 . . . . . . . 
+    . . . . . . 1 1 1 1 1 . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Hole)
+hole.setPosition(960, 105)
+scene.cameraFollowSprite(golfball.sprite)
